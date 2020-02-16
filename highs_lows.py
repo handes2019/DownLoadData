09a3dd02dc5b8961daca9a6a1de75c7d90bb46ed
handes <2451322819@qq.com>
2020-02-16 16:50:51 +0800

    2020/2/16

--- highs_lows.py
+++ highs_lows.py
@@ -1,10 +1,17 @@
 import csv
 
+# 从文件中获取最高气温
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     # print(header_row)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    highs = []
+    for row in reader:
+        highs.append(row[1])
+
+    print(highs)
