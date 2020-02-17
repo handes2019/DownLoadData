0c6a66548f62f25abfcdbebc9b990a5694f52ec7
handes <2451322819@qq.com>
2020-02-17 15:57:19 +0800

    2020/2/16

--- btc_master/btc_close_2017.py
+++ btc_master/btc_close_2017.py
@@ -1,5 +1,8 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 try:
     # Python 2.x 版本
     from urllib2 import urlopen
@@ -8,12 +11,15 @@ except ImportError:
     from urllib.request import urlopen
 import json
 
-json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
-response = urlopen(json_url)
+#json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
+#response = urlopen(json_url)
 #读取数据
-req = response.read()
+#req = response.read()
+with open('btc_close_2017.json') as f:
+    req = f.read()
+
 #将数据写入文件
-with open('btc_close_2017_urllib.json', 'wb') as f:
+with open('btc_close_2017_urllib.json', 'w') as f:
     f.write(req)
 
 #加载json格式
